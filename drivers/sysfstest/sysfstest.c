08e92b491681d7ba645af9f83df23a6c25cf6994
Alex.xu <xuleilx@gmail.com>
2022-01-18 15:12:41 +0800

    [Drivers][sysfs]add kobject_uevent

--- drivers/sysfstest/sysfstest.c
+++ drivers/sysfstest/sysfstest.c
@@ -40,15 +40,14 @@ static int __init chrdev_init(void)
 
     chrdev_kob = kobject_create_and_add("obj_test",&chrdev_kset->kobj);
 	if (!chrdev_kob){
-        kset_unregister(chrdev_kset);
         return -ENOMEM;
     }
-		
+	chrdev_kob->kset = chrdev_kset;
 	rc = sysfs_create_group(chrdev_kob, &chrdev_attr_group);
 	if (rc){
-        kobject_put(chrdev_kob);
-        kset_unregister(chrdev_kset);
+		kobject_put(chrdev_kob);
     }
+    kobject_uevent(chrdev_kob,KOBJ_ADD);
 	return rc;
 
 }
@@ -57,8 +56,9 @@ module_init(chrdev_init);
 static void __exit chrdev_exit(void)
 {
     printk("chrdev exit\n");
-    kobject_put(chrdev_kob);
-    kset_unregister(chrdev_kset);
+	kobject_uevent(chrdev_kob,KOBJ_REMOVE);
+	sysfs_remove_group(chrdev_kob, &chrdev_attr_group);
+	kobject_put(chrdev_kob);
 }
 module_exit(chrdev_exit);
 
